aadb49fa3fb4bf88ad263ad115ee1b385a22fce6
kwiniarski97 <kwiniarski97@gmail.com>
2020-02-08 14:52:37 +0000

    Update master to output generated at 48a1ce4

---
+++ precache-manifest.1f12bd8495b3531ad0be4440d294b812.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc938731edbec74b1a8",
+    "url": "/css/app.ff35708b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "849ca58c8959caa7b148124fcdac9995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b825e22117866783ff39",
+    "url": "/js/about.df2f85af.js"
+  },
+  {
+    "revision": "8fc938731edbec74b1a8",
+    "url": "/js/app.894fc73e.js"
+  },
+  {
+    "revision": "a35377ca17eef60a612d",
+    "url": "/js/chunk-vendors.490d9600.js"
+  },
+  {
+    "revision": "76ebde1bbbf34ca9da5d311e7e3e0bdc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
