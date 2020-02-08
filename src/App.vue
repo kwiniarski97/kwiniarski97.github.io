48a1ce4093a830aa48ff952b836b47cfc5b76e3f
Karol <kwiniarski97@gmail.com>
2020-02-08 15:50:34 +0100

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <header id="header"></header>
+    <Navigation id="nav" />
+    <main id="main">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation";
+
+export default {
+  name: "App",
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Lato&display=swap");
+@import "scss/variables";
+#app {
+  display: grid;
+  grid-template-areas:
+    ". header ."
+    "nav main ad";
+  grid-template-columns: 1fr 4fr 1fr;
+  grid-template-rows: 40px auto;
+  grid-gap: 16px;
+}
+
+#header {
+  grid-area: header;
+}
+
+#nav {
+  grid-area: nav;
+}
+
+#main {
+  grid-area: main;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+  <nav class="navigation">
+    <ul class="navigation__list">
+      <li
+        v-for="(link, index) in links"
+        :key="`link-${index}`"
+        :style="link.extraStyle"
+      >
+        <router-link :to="link.link">
+          {{ link.text }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Navigation extends Vue {
+  links: Link[] = [
+    {
+      text: "Home",
+      link: "/",
+      extraStyle: { fontWeight: "bold" }
+    },
+    {
+      text: "css-grid",
+      link: "/about"
+    }
+  ];
+}
+
+interface Link {
+  text: string;
+  link: string;
+  extraStyle?: { [property: string]: string };
+}
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+  text-align: right;
+
+  &__list {
+    list-style: none;
+  }
+}
+</style>
